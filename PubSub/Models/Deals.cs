fe61125a20689cbc005ee670de356220a5fd4a1f
Darklure <jeremy.nooney@gmail.com>
2020-09-28 18:59:48 -0400

    Latest code for working on odd ball scenarios, duplicates, and images.

---
+++ PubSub/Models/Deals.cs
@@ -0,0 +1,16 @@
+﻿using PubSub.Models.GraphQL.PublixAd;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PubSub.Models
+{
+    public struct Deals
+    {
+        public Rollover rollover { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+    }
+}
---
+++ PubSub/Utils/ArrayUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PubSub.Utils
+{
+    public class ArrayUtil
+    {
+
+        public static bool IsDuplicate(string[] data, int currentIndex)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == data[currentIndex])
+                {
+                    return true;   
+                }
+            }
+
+            return false;
+        }
+    }
+}
